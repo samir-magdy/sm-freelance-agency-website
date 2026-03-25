6675d3232e7dd4b7a24ed064bc6f261182b7df62
samir-magdy <samir.magdy.dev@protonmail.com>
2026-03-26 01:45:28 +0200

    fix robots and sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { MetadataRoute } from "next";
 import { SITE_URL } from "./data/translations/lang";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const modifyDate = "2026-03-14";
+  const modifyDate = "2026-03-26";
   return [
     {
       url: SITE_URL,
