c77f560c4eeae8547633a36701a6ec76c231c18d
samir-magdy <samir.magdy.dev@protonmail.com>
2026-06-08 01:37:01 +0300

    guide

--- app/data/guides/index.js
+++ app/data/guides/index.js
@@ -1,13 +1,13 @@
 import whyYourBusinessNeedsAWebsite from "./why-your-business-needs-a-website";
 import howMuchDoesAWebsiteCost from "./website-cost-in-egypt";
 import webDesignVsWebsiteBuilder from "./diy-vs-professional-web-design";
-// import howToPickTheRightWebDesignCompany from "./choose-web-design-company-egypt";
+import howToPickTheRightWebDesignCompany from "./choose-web-design-company-egypt";
 
 const resources = [
   whyYourBusinessNeedsAWebsite,
   webDesignVsWebsiteBuilder,
   howMuchDoesAWebsiteCost,
-  // howToPickTheRightWebDesignCompany,
+  howToPickTheRightWebDesignCompany,
 ];
 
 export default resources;
