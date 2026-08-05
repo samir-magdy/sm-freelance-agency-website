3eeb4ff80fed4e46bad635851239afddae77c245
samir-magdy <samir.magdy.dev@pm.me>
2026-08-05 20:55:01 +0300

    usss

--- app/components/sections/FAQSection.tsx
+++ app/components/sections/FAQSection.tsx
@@ -43,7 +43,7 @@ export default function FAQSection({ lang }: FAQSectionProps) {
               >
                 <span className="relative inline-block shrink-0 w-5 h-5 me-5" aria-hidden="true">
                   <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3 h-0.5 rounded-full bg-icon" />
-                  <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-0.5 h-3 rounded-full bg-icon [details[open]_&]:rotate-90" />
+                  <span className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-0.5 h-3 rounded-full bg-icon transition-transform duration-200 ease-out [details[open]_&]:rotate-90" />
                 </span>
 
                 <h3 className="text-content-heading font-normal tracking-wide text-base sm:text-subheading">
