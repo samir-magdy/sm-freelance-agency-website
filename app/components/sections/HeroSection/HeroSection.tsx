6e9c1ed7815a0298cc9f6b8ac2c28fccf43989b0
samir-magdy <samir.magdy.dev@pm.me>
2026-08-23 15:48:12 +0300

    coolsasd

--- app/components/sections/HeroSection/HeroSection.tsx
+++ app/components/sections/HeroSection/HeroSection.tsx
@@ -44,7 +44,7 @@ export default function HeroSection({ lang }: HeroSectionProps) {
         />
 
         <p
-          className="mt-5 mb-3 text-content-muted font-normal text-balance text-[clamp(1.1rem,6vw,1.8rem)] leading-[1.55] max-w-[34rem] 2xl:max-w-[35rem] rtl:mt-2.5 rtl:text-[clamp(0.9rem,4vw,1.5rem)] rtl:leading-[2.15] sm:mt-6 sm:text-center sm:leading-[1.6] sm:rtl:mt-4 sm:rtl:leading-loose lg:text-[clamp(1.1rem,5vw,1.6rem)] lg:mb-1 lg:max-w-[30rem] lg:rtl:text-[clamp(0.9rem,4vw,1.35rem)] 2xl:text-[clamp(1.1rem,5vw,1.79rem)] 2xl:rtl:text-[clamp(0.9rem,4vw,1.51rem)]"
+          className="mt-5 mb-3 text-content-muted font-normal text-balance text-[clamp(1.1rem,5.5vw,1.8rem)] leading-[1.55] max-w-[34rem] 2xl:max-w-[35rem] rtl:mt-2.5 rtl:text-[clamp(0.9rem,4vw,1.5rem)] rtl:leading-[2.15] sm:mt-6 sm:text-center sm:leading-[1.6] sm:rtl:mt-4 sm:rtl:leading-loose lg:text-[clamp(1.1rem,5vw,1.6rem)] lg:mb-1 lg:max-w-[30rem] lg:rtl:text-[clamp(0.9rem,4vw,1.35rem)] 2xl:text-[clamp(1.1rem,5vw,1.79rem)] 2xl:rtl:text-[clamp(0.9rem,4vw,1.51rem)]"
           dangerouslySetInnerHTML={{ __html: hero.valueProp[lang] }}
         />
 
